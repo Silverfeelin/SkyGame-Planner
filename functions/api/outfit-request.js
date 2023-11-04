8e17a4bb9c58a5e2b2918f00bdcf8d7d8f35c157
Silverfeelin <Silverfeelin@gmail.com>
2023-11-04 20:09:31 +0100

    Attempt 1 outfit request server function

--- functions/api/outfit-request.js
+++
@@ -1,7 +0,0 @@
-export function onRequestGet(context) {
-  return new Response('Hello world');
-}
-
-export function onRequestPost(context) {
-  return new Response('Hello world');
-}
---
+++ functions/api/outfit-request.ts
@@ -0,0 +1,55 @@
+import { nanoid } from '../nanoid';
+
+interface Env {
+	REQUESTS: KVNamespace;
+}
+
+const invalidRequest = () => new Response('Invalid request.', { status: 400 });
+
+/** Fetch a request and return the stored data. */
+export const onRequestGet: PagesFunction<Env> = async (context) => {
+  // Get request key.
+  const url = new URL(context.request.url);
+  const key = url.searchParams.get('key');
+  if (!key) { return invalidRequest(); }
+
+  // Get request data.
+  const request = await context.env.REQUESTS.get(key, 'text');
+
+  // Return request data.
+  return new Response(request);
+}
+
+/** Request body for storing an outfit request under a short key. */
+interface OutfitRequest {
+  /** "Have": Items marked as available. */
+  h: string;
+  /** "Red": Items marked with red. */
+  r: string;
+  /** "Green": Items marked with green. */
+  g: string;
+  /** "Blue": Items marked with blue. */
+  b: string;
+}
+
+/** Save a request and return a random storage key.  */
+export const onRequestPost: PagesFunction<Env> = async (context) => {
+  // Get request body.
+  const json = await context.request.json() as OutfitRequest;
+  const jsonKeys = Object.keys(json);
+  const validKeys = new Set(['h', 'r', 'g', 'b']);
+
+  // Quick data sanitization.
+  if (jsonKeys.some(k => !validKeys.has(k) || typeof(json[k]) !== 'string' || json[k]?.length > 10000)) {
+    return invalidRequest();
+  }
+
+  // Store request
+  const key = nanoid();
+  context.env.REQUESTS.put(key, JSON.stringify(json), { expirationTtl: 86400 });
+  const response = { key };
+
+  // Return key to site.
+  return new Response(JSON.stringify(response));
+
+}
