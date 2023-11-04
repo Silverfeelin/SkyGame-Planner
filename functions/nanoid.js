9850f26232547664e2b347d60528b73de2113ab7
Silverfeelin <Silverfeelin@gmail.com>
2023-11-04 20:13:01 +0100

    Fix nanoid export

--- functions/nanoid.js
+++ functions/nanoid.js
@@ -1,4 +1,18 @@
 // nanoid - https://github.com/ai/nanoid
 // MIT License - https://github.com/ai/nanoid/blob/main/LICENSE
-// https://cdnjs.cloudflare.com/ajax/libs/nanoid/5.0.2/index.browser.min.js
-export{urlAlphabet}from"./url-alphabet/index.js";let random=t=>crypto.getRandomValues(new Uint8Array(t)),customRandom=(n,t,a)=>{let l=(2<<Math.log(n.length-1)/Math.LN2)-1,m=-~(1.6*l*t/n.length);return(e=t)=>{let o="";for(;;){var r=a(m);let t=m;for(;t--;)if((o+=n[r[t]&l]||"").length===e)return o}}},customAlphabet=(t,e=21)=>customRandom(t,e,random),nanoid=(t=21)=>crypto.getRandomValues(new Uint8Array(t)).reduce((t,e)=>t+=(e&=63)<36?e.toString(36):e<62?(e-26).toString(36).toUpperCase():62<e?"-":"_","");export{random,customRandom,customAlphabet,nanoid};
+
+// https://cdnjs.cloudflare.com/ajax/libs/nanoid/5.0.2/index.browser.js
+export let nanoid = (size = 21) =>
+  crypto.getRandomValues(new Uint8Array(size)).reduce((id, byte) => {
+    byte &= 63
+    if (byte < 36) {
+      id += byte.toString(36)
+    } else if (byte < 62) {
+      id += (byte - 26).toString(36).toUpperCase()
+    } else if (byte > 62) {
+      id += '-'
+    } else {
+      id += '_'
+    }
+    return id
+  }, '')
