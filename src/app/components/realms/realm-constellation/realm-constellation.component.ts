4524ada977ef09aee86de884930c7ed5cff9fca8
Silverfeelin <Silverfeelin@gmail.com>
2023-12-04 16:02:25 +0100

    Improve constellation

--- src/app/components/realms/realm-constellation/realm-constellation.component.ts
+++ src/app/components/realms/realm-constellation/realm-constellation.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IRealm } from 'src/app/interfaces/realm.interface';
 import { ISpirit } from 'src/app/interfaces/spirit.interface';
@@ -10,7 +10,7 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./realm-constellation.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RealmConstellationComponent implements OnInit {
+export class RealmConstellationComponent implements OnInit, OnChanges {
   @Input() realm!: IRealm;
 
   @ViewChild('canvas', { static: true }) canvas!: ElementRef<HTMLCanvasElement>;
@@ -27,10 +27,9 @@ export class RealmConstellationComponent implements OnInit {
   targetKeyframe = 0;
   realmIndex = 0;
   maxRealmIndex = 5;
-
-  direction: number = 1;
   interval?: number;
   swapping = false;
+  disposing = false;
 
   constructor(
     private readonly _dataService: DataService,
@@ -43,15 +42,44 @@ export class RealmConstellationComponent implements OnInit {
   ngOnInit(): void {
     this.realmIndex = this.realms.findIndex(r => r.guid === this.realm.guid);
     this.keyframe = this.keyframes[this.realmIndex];
-
     this.initializeFrames();
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['realm']) {
+      this.realmIndex = this.realms.findIndex(r => r.guid === this.realm.guid);
+      this.animateToRealm();
+    }
+  }
+
+  gotoSpirit(spirit: ISpirit): void {
+    this.spiritClicked.emit(spirit);
+  }
+
+  jumpConstellation(i: number): void {
+    if (this.swapping) { return; }
+    this.realmIndex = i;
+    this.animateToRealm();
+
+    this.realmChanged.emit(this.realms[this.realmIndex]);
+  }
+
+  scrollConstellation(lr: number): void {
+    if (this.swapping) { return; }
+    this.realmIndex = Math.max(0, Math.min(this.realmIndex + lr, this.maxRealmIndex));
+    this.animateToRealm();
+
+    this.realmChanged.emit(this.realms[this.realmIndex]);
+  }
+
   private initializeFrames(): void {
+    // Frames are already loaded. Render the current constellation.
     if (RealmConstellationComponent.framesLoaded) {
       setTimeout(() => this.renderCurrentFrame());
       return;
     }
+
+    // Frame are being loaded.
     if (RealmConstellationComponent.frames) { return; }
 
     const frames: Array<HTMLImageElement> = [];
@@ -74,58 +102,57 @@ export class RealmConstellationComponent implements OnInit {
       promises.push(promise);
     }
 
-
+    // When all frames have loaded, render the current frame.
     Promise.all(promises).then(() => {
       RealmConstellationComponent.framesLoaded = true;
       this.renderCurrentFrame();
     });
   }
 
-  gotoSpirit(spirit: ISpirit): void {
-    this.spiritClicked.emit(spirit);
-  }
-
-  scrollTo(lr: number): void {
-    if (this.swapping) { return; }
-    this.animateTo(lr);
-  }
-
   private renderCurrentFrame(): void {
     const ctx = this.canvas.nativeElement.getContext('2d');
-      if (!ctx) { return; }
+    if (!ctx) { return; }
     const w = this.canvas.nativeElement.width;
     const h = this.canvas.nativeElement.height;
     ctx.clearRect(0, 0, w, h);
+
     const img = RealmConstellationComponent.frames[this.keyframe];
     ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, w, h);
     this._changeDetectorRef.markForCheck();
   }
 
-  private animateTo(lr: number): void {
+  private animateToRealm(): void {
     if (this.swapping) { return; }
+    this.targetKeyframe = this.keyframes[this.realmIndex];
+
     const ctx = this.canvas.nativeElement.getContext('2d');
     if (!ctx) { return; }
 
-    this.realmIndex = Math.max(0, Math.min(this.realmIndex + lr, this.maxRealmIndex));
-
-    this.direction = lr;
-    this.targetKeyframe = this.keyframes[this.realmIndex];
-
     // Prevent double animation loop.
     if (this.interval) { return; }
-
+    // Already at target.
     if (this.keyframe === this.targetKeyframe) { return; }
+
+    // Start animating.
     this.interval = window.setInterval(() => {
+      // Move keyframe 1 towards target.
+      if (this.keyframe !== this.targetKeyframe) {
+        this.keyframe += this.keyframe < this.targetKeyframe ? 1 :  -1;
+        this.keyframe = Math.max(0, Math.min(this.keyframe, this.keyframes.at(-1)!));
+      }
+
+      // Render frame
       const w = this.canvas.nativeElement.width;
       const h = this.canvas.nativeElement.height;
-
-      this.keyframe += this.direction;
       const img = RealmConstellationComponent.frames[this.keyframe];
       ctx.clearRect(0, 0, w, h);
       ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, w, h);
+
+      // Stop animation.
       if (this.keyframe === this.targetKeyframe) {
         this.doneAnimating();
       }
+
       this._changeDetectorRef.markForCheck();
     }, 30);
   }
@@ -134,7 +161,6 @@ export class RealmConstellationComponent implements OnInit {
     window.clearInterval(this.interval);
     this.interval = undefined;
     this.swapping = true;
-    this.realmChanged.emit(this.realms[this.realmIndex]);
     setTimeout(() => {
       this.swapping = false;
       this._changeDetectorRef.markForCheck();
