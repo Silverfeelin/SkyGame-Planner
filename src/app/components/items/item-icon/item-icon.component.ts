a1f5cca7c9a7ac0249ba6a4a17b411e76a4fb99f
Silverfeelin <Silverfeelin@gmail.com>
2023-12-07 21:02:49 +0100

    Item goto highlight

--- src/app/components/items/item-icon/item-icon.component.ts
+++ src/app/components/items/item-icon/item-icon.component.ts
@@ -3,6 +3,7 @@ import { SubscriptionLike } from 'rxjs';
 import { IItem, ItemSize, ItemType } from 'src/app/interfaces/item.interface';
 import { DebugService } from 'src/app/services/debug.service';
 import { EventService } from 'src/app/services/event.service';
+import { HighlightType } from 'src/app/types/highlight';
 
 @Component({
   selector: 'app-item-icon',
@@ -13,10 +14,15 @@ import { EventService } from 'src/app/services/event.service';
 export class ItemIconComponent implements OnInit, OnChanges, OnDestroy {
   @Input() item!: IItem;
   @Input() size: ItemSize = 'default';
-  @Input() highlight?: boolean;
-  @HostBinding('class.hoverable')
-  @Input() hoverHighlight = true;
   @Input() showSubIcons?: boolean;
+
+  /** Forces icon to be opaque instead of checking if it's unlocked. */
+  @Input() opaque?: boolean;
+
+  @HostBinding('class.hoverable')
+  @Input() hoverGlow = true;
+  /** Type of highlight border */
+  @Input() glowType?: HighlightType = 'default';
 
   @HostBinding('attr.data-size')
   _size: ItemSize = 'default';
