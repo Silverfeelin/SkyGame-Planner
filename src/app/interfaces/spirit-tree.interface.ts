3a4879529658686a6780c76f4a84e2af99e70754
Silverfeelin <Silverfeelin@gmail.com>
2024-03-26 22:00:11 +0100

    TS 110

--- src/app/interfaces/spirit-tree.interface.ts
+++ src/app/interfaces/spirit-tree.interface.ts
@@ -8,6 +8,8 @@ import { ITravelingSpirit } from "./traveling-spirit.interface";
 export interface ISpiritTreeConfig extends IConfig<ISpiritTree> {}
 
 export interface ISpiritTree extends IGuid {
+  draft?: boolean;
+
   /// References ///
   node: INode;
   ts?: ITravelingSpirit;
