1e913f29896dd2168ed3b0e6bfb86ef425f3e58a
Silverfeelin <Silverfeelin@gmail.com>
2024-06-12 01:03:56 +0200

    Nesting challenges

--- src/app/components/shops/shops.component.ts
+++ src/app/components/shops/shops.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { IIAP } from 'src/app/interfaces/iap.interface';
 import { IShop } from 'src/app/interfaces/shop.interface';
+import { ISpiritTree } from 'src/app/interfaces/spirit-tree.interface';
+import { ISpirit } from 'src/app/interfaces/spirit.interface';
 import { DataService } from 'src/app/services/data.service';
 import { IAPService } from 'src/app/services/iap.service';
 
@@ -16,14 +18,25 @@ export class ShopsComponent {
   highlightIap?: string;
   highlightNode?: string;
 
+  challengeSpirits: Array<ISpirit>;
+  nestingWorkshop?: IShop;
+
   constructor(
     private readonly _dataService: DataService,
     private readonly _iapService: IAPService,
     _route: ActivatedRoute
   ) {
-    const shops = this._dataService.shopConfig.items.filter(s => s.permanent);
+    const shops = this._dataService.shopConfig.items.filter(s => {
+      if (!s.permanent) { return false; }
+      if (s.guid !== 'he4MHA7_uC') { return true; }
+      this.nestingWorkshop = s;
+      return false;
+    });
     this.iapShops = shops.filter(s => s.iaps?.length);
     this.shops = shops.filter(s => s.itemList);
+
+    this.challengeSpirits = [ 'os6ryCdFZ5', 'Gp-hW_NCv_', 'IhAh5oTvF8' ].map(g => this._dataService.guidMap.get(g)!) as Array<ISpirit>;
+
     _route.queryParamMap.subscribe(p => this.onQueryChanged(p));
   }
 
