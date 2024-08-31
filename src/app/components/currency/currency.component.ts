489b79ada7a2c5b321cd61500baf670d3ebf88f2
Silverfeelin <Silverfeelin@gmail.com>
2024-08-31 02:59:09 +0200

    currency decimals
    Closes #156

--- src/app/components/currency/currency.component.ts
+++ src/app/components/currency/currency.component.ts
@@ -27,6 +27,7 @@ export class CurrencyComponent {
   @ViewChild('inpSc', { static: false }) inpSc!: ElementRef<HTMLInputElement>;
   @ViewChildren('inpEc') inpEc!: QueryList<ElementRef<HTMLInputElement>>;
 
+  inpCurrencies: IStorageCurrencies;
   currencies: IStorageCurrencies;
   ongoingSeason?: ISeason;
   ongoingEventInstances: Array<IEventInstance>;
@@ -37,7 +38,8 @@ export class CurrencyComponent {
     private readonly _dataService: DataService,
     private readonly _storageService: StorageService
   ) {
-    this.currencies = _storageService.getCurrencies();
+    this.inpCurrencies = this._storageService.getCurrencies();
+    this.currencies = this._storageService.getCurrencies();
     let changed = false;
 
     this.ongoingSeason = DateHelper.getActive(this._dataService.seasonConfig.items);
@@ -86,9 +88,13 @@ export class CurrencyComponent {
     this.currencyInputChanged();
   }
 
-  onCurrencyInputBlur(evt: Event): void {
+  onCurrencyInputBlur(evt: Event, isAsc = false): void {
     const target = evt.target as HTMLInputElement;
-    const value = parseInt(target.value, 10) || 0;
+    let value = isAsc ? this.parseDecimal(target.value) : this.parseInt(target.value);
+    if (isAsc && value % 0.25 !== 0) {
+      value = Math.round(value * 4) / 4;
+      target.value = value.toString();
+    }
     if (value <= 0) {
       target.value = '0';
     } else if (value > 99999) {
@@ -103,30 +109,51 @@ export class CurrencyComponent {
   currencyInputChanged(): void {
     // Candles
     const targetC = this.inpC.nativeElement;
-    this.currencies.candles = Math.min(99999, Math.max(parseInt(targetC.value, 10) || 0, 0));
+    this.currencies.candles = Math.min(99999, Math.max(this.parseInt(targetC.value), 0));;
+    if (this.currencies.candles !== this.parseDecimal(targetC.value)) {
+      targetC.value = this.currencies.candles.toString();
+    }
     // Hearts
     const targetH = this.inpH.nativeElement;
-    this.currencies.hearts = Math.min(99999, Math.max(parseInt(targetH.value, 10) || 0, 0));
+    this.currencies.hearts = Math.min(99999, Math.max(this.parseInt(targetH.value), 0));
+    if (this.currencies.hearts !== this.parseDecimal(targetH.value)) {
+      targetH.value = this.currencies.hearts.toString();
+    }
     // Ascended candles
     const targetAc = this.inpAc.nativeElement;
-    this.currencies.ascendedCandles = Math.min(99999, Math.max(parseInt(targetAc.value, 10) || 0, 0));
+    this.currencies.ascendedCandles = Math.min(99999, Math.max(this.parseDecimal(targetAc.value), 0));
+    if (this.currencies.ascendedCandles !== this.parseDecimal(targetAc.value)) {
+      targetAc.value = this.currencies.ascendedCandles.toString();
+    }
     // Gift passes
     const targetSp = this.inpSp.nativeElement;
-    this.currencies.giftPasses = Math.min(99999, Math.max(parseInt(targetSp.value, 10) || 0, 0));
+    this.currencies.giftPasses = Math.min(99999, Math.max(this.parseInt(targetSp.value), 0));
+    if (this.currencies.giftPasses !== this.parseDecimal(targetSp.value)) {
+      targetSp.value = this.currencies.giftPasses.toString();
+    }
 
     if (this.ongoingSeason && this.inpSc) {
       const targetSc = this.inpSc.nativeElement;
-      this.currencies.seasonCurrencies[this.ongoingSeason.guid].candles = Math.min(99999, Math.max(parseInt(targetSc.value, 10) || 0, 0));
+      this.currencies.seasonCurrencies[this.ongoingSeason.guid].candles = Math.min(99999, Math.max(this.parseInt(targetSc.value), 0));
+      if (this.currencies.seasonCurrencies[this.ongoingSeason.guid].candles !== this.parseDecimal(targetSc.value)) {
+        targetSc.value = this.currencies.seasonCurrencies[this.ongoingSeason.guid].candles.toString();
+      }
     }
 
     const inpsEc = this.inpEc.toArray();
     if (inpsEc.length) {
       for (const [i, instance] of this.ongoingEventInstances.entries()) {
         const targetEc = inpsEc[i].nativeElement;
-        this.currencies.eventCurrencies[instance.guid].tickets = Math.min(99999, Math.max(parseInt(targetEc.value, 10) || 0, 0));
+        this.currencies.eventCurrencies[instance.guid].tickets = Math.min(99999, Math.max(this.parseInt(targetEc.value), 0));
+        if (this.currencies.eventCurrencies[instance.guid].tickets !== this.parseDecimal(targetEc.value)) {
+          targetEc.value = this.currencies.eventCurrencies[instance.guid].tickets.toString();
+        }
       }
     }
 
     this._storageService.setCurrencies(this.currencies);
   }
+
+  private parseInt(value?: string): number { return parseInt(value || '', 10) || 0; }
+  private parseDecimal(value?: string): number { return +(value?.replace(',', '') || 0) || 0; }
 }
