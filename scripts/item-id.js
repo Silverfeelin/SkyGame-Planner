1f510a6dd437f251f0504eeec9fff50f535f1e28
Silverfeelin <Silverfeelin@gmail.com>
2024-09-18 22:46:45 +0200

    Finish Duets Guide tree
    Closes #173

--- scripts/item-id.js
+++ scripts/item-id.js
@@ -6,12 +6,4 @@ const rawdata = fs.readFileSync(path.join(__dirname, '../src/assets/data/items.j
 const itemConfig = json5.parse(rawdata);
 const maxId = Math.max(...itemConfig.items.map(i => i.id));
 
-try {
-  let wikiJs = fs.readFileSync(path.join(__dirname, './userscripts/wiki_item.js'), 'utf8');
-  wikiJs = wikiJs.replace(/\/\*S-ID\*\/(\d+)/, `/*S-ID*/${maxId + 1}`);
-  fs.writeFileSync(path.join(__dirname, './userscripts/wiki_item.js'), wikiJs);
-} catch (e) {
-  console.error(e);
-}
-
 console.log(maxId + 1);
