bf5f5e2da563e4403f5cd6a99c04c6ca2ba59f91
Silverfeelin <Silverfeelin@gmail.com>
2024-11-27 13:34:26 +0100

    PWA setting
    Prevent annoying popup. Add info & button to settings.

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, effect, OnDestroy, signal } from '@angular/core';
 import { DataService, ITrackables } from 'src/app/services/data.service';
 import { DateHelper } from 'src/app/helpers/date-helper';
 import { SettingService } from 'src/app/services/setting.service';
@@ -10,6 +10,7 @@ import { NgFor, LowerCasePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MatIcon } from '@angular/material/icon';
 import { ITheme, setTheme, themes } from 'src/themes';
+import { CardComponent } from "../layout/card/card.component";
 
 interface IExport {
   version: string;
@@ -19,14 +20,16 @@ interface IExport {
   };
 }
 
+const signalPwa = signal<any>(undefined);
+
 @Component({
     selector: 'app-settings',
     templateUrl: './settings.component.html',
     styleUrls: ['./settings.component.less'],
     standalone: true,
-    imports: [MatIcon, RouterLink, NgFor, LowerCasePipe, DateTimePipe]
+    imports: [MatIcon, RouterLink, NgFor, LowerCasePipe, DateTimePipe, CardComponent]
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnDestroy {
   storageProviderName: string;
   unlockConnectedNodes: boolean;
   today = DateTime.now();
@@ -36,10 +39,13 @@ export class SettingsComponent {
   themes = themes;
   wikiNewTab = false;
 
+  pwaInstallPrompt?: any = (window as any).pwaInstallPrompt;
+
   constructor(
     private readonly _dataService: DataService,
     private readonly _storageService: StorageService,
-    private readonly _settingService: SettingService
+    private readonly _settingService: SettingService,
+    private readonly _changeDetectorRef: ChangeDetectorRef
   ) {
     this.storageProviderName = this._storageService.getProviderName();
     this.dateFormats = DateHelper.displayFormats;
@@ -47,6 +53,29 @@ export class SettingsComponent {
     this.wikiNewTab = _settingService.wikiNewTab;
     this.currentTheme = localStorage.getItem('theme') || '';
     this.unlockConnectedNodes = _storageService.getKey('tree.unlock-connected') !== '0';
+
+    addEventListener('beforeinstallprompt', this.beforeInstallPrompt);
+
+    effect(() => {
+      this.pwaInstallPrompt ??= signalPwa();
+      _changeDetectorRef.markForCheck();
+    });
+  }
+
+  ngOnDestroy(): void {
+    removeEventListener('beforeinstallprompt', this.beforeInstallPrompt);
+  }
+
+  beforeInstallPrompt(evt: Event): void {
+    evt.preventDefault();
+    (window as any).pwaInstallPrompt = evt;
+    signalPwa.set(evt);
+  }
+
+  async promptPwa(): Promise<void> {
+    if (!this.pwaInstallPrompt) { return; }
+    const result = await this.pwaInstallPrompt.prompt();
+    console.log(result);
   }
 
   import(): void {
