bc6c2f5c52d9bb2eb0d1d6db5833eed58f938b7a
A1rPun <thombakker@gmail.com>
2025-05-08 20:49:35 +0200

    Add friends component

---
+++ src/app/components/friends/friends.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ActivatedRoute, ParamMap, RouterLink } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+import { WikiLinkComponent } from "../util/wiki-link/wiki-link.component";
+import { SpiritTreeComponent } from "../spirit-tree/spirit-tree.component";
+import { DataService } from '@app/services/data.service';
+
+@Component({
+    selector: 'app-friends',
+    templateUrl: './friends.component.html',
+    styleUrls: ['./friends.component.less'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [MatIcon, RouterLink, WikiLinkComponent, SpiritTreeComponent]
+})
+export class FriendsComponent {
+  highlightTree?: string;
+  highlightItem?: string;
+
+  constructor(
+    private readonly _dataService: DataService,
+    private readonly _route: ActivatedRoute
+  ) {
+    _route.queryParamMap.subscribe(p => this.onQueryChanged(p));
+    _route.paramMap.subscribe(p => this.onParamsChanged(p));
+  }
+
+  onQueryChanged(p: ParamMap): void {
+    this.highlightTree = p.get('highlightTree') || undefined;
+    this.highlightItem = p.get('highlightItem') || undefined;
+  }
+
+  onParamsChanged(params: ParamMap): void {
+    const guid = params.get('guid');
+
+  }
+}
