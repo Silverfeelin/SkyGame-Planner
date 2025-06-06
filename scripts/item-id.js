604d773db50963539d8758279fe9db16e2746452
Silverfeelin <Silverfeelin@gmail.com>
2025-06-06 16:57:09 +0200

    JSON5>jsonc-parser. Closes #307

--- scripts/item-id.js
+++ scripts/item-id.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const json5 = require('json5');
+const jsonc = require('jsonc-parser');
 
 // nanoid - https://github.com/ai/nanoid
 // MIT License - https://github.com/ai/nanoid/blob/main/LICENSE
@@ -21,8 +21,8 @@ const nanoid = (size = 21) =>
   }, '')
 
 const getItemId = () => {
-  const rawdata = fs.readFileSync(path.join(__dirname, '../src/assets/data/items.json'));
-  const itemConfig = json5.parse(rawdata);
+  const rawdata = fs.readFileSync(path.join(__dirname, '../src/assets/data/items.json'), 'utf8');
+  const itemConfig = jsonc.parse(rawdata);
   const maxId = Math.max(...itemConfig.items.map(i => i.id || 0));
   return maxId + 1;
 };
@@ -30,7 +30,7 @@ const getItemId = () => {
 // Check and apply IDs to unsorted.json.
 let id = getItemId();
 const itemsFilePath = path.resolve(__dirname, '../src/assets/data/items/unsorted.json');
-const items = json5.parse(fs.readFileSync(itemsFilePath, 'utf8'));
+const items = jsonc.parse(fs.readFileSync(itemsFilePath, 'utf8'));
 let changed = false;
 items.forEach((item, i) => {
   console.log(item);
