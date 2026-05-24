682b3f7b2b8f4a3ec7edcc2d0bd74e0a952cdb27
Silverfeelin <Silverfeelin@gmail.com>
2026-05-24 16:31:40 +0200

    item grid template fixes

--- src/app/redesign/item/grid/atmos-item-grid.component.ts
+++ src/app/redesign/item/grid/atmos-item-grid.component.ts
@@ -7,25 +7,25 @@ import { DataService } from '@app/services/data.service';
 import { ItemHelper } from '@app/helpers/item-helper';
 import { IItem, ItemType } from 'skygame-data';
 
-const CATEGORIES: ReadonlyArray<{ type: ItemType; label: string; icon: string }> = [
-  { type: ItemType.Outfit,        label: 'Outfit',          icon: 'checkroom' },
-  { type: ItemType.Shoes,         label: 'Shoes',           icon: 'footprint' },
-  { type: ItemType.OutfitShoes,   label: 'Outfit + Shoes',  icon: 'checkroom' },
-  { type: ItemType.Mask,          label: 'Mask',            icon: 'theater_comedy' },
-  { type: ItemType.FaceAccessory, label: 'Face Accessory',  icon: 'face' },
-  { type: ItemType.Necklace,      label: 'Necklace',        icon: 'diamond' },
-  { type: ItemType.Hair,          label: 'Hair',            icon: 'person' },
-  { type: ItemType.HairAccessory, label: 'Hair Accessory',  icon: 'face_retouching_natural' },
-  { type: ItemType.HeadAccessory, label: 'Hat',             icon: 'sports_baseball' },
-  { type: ItemType.Cape,          label: 'Cape',            icon: 'dry_cleaning' },
-  { type: ItemType.Held,          label: 'Held',            icon: 'back_hand' },
-  { type: ItemType.Furniture,     label: 'Furniture',       icon: 'chair' },
-  { type: ItemType.Prop,          label: 'Prop',            icon: 'extension' },
-  { type: ItemType.Emote,         label: 'Emote',           icon: 'sentiment_satisfied' },
-  { type: ItemType.Stance,        label: 'Stance',          icon: 'accessibility_new' },
-  { type: ItemType.Call,          label: 'Call',            icon: 'campaign' },
-  { type: ItemType.Music,         label: 'Music',           icon: 'music_note' },
-  { type: ItemType.Spell,         label: 'Spell',           icon: 'auto_fix_high' },
+const CATEGORIES: ReadonlyArray<{ type: ItemType; icon: string }> = [
+  { type: ItemType.Outfit,        icon: 'checkroom' },
+  { type: ItemType.Shoes,         icon: 'footprint' },
+  { type: ItemType.OutfitShoes,   icon: 'checkroom' },
+  { type: ItemType.Mask,          icon: 'theater_comedy' },
+  { type: ItemType.FaceAccessory, icon: 'face' },
+  { type: ItemType.Necklace,      icon: 'diamond' },
+  { type: ItemType.Hair,          icon: 'person' },
+  { type: ItemType.HairAccessory, icon: 'face_retouching_natural' },
+  { type: ItemType.HeadAccessory, icon: 'sports_baseball' },
+  { type: ItemType.Cape,          icon: 'dry_cleaning' },
+  { type: ItemType.Held,          icon: 'back_hand' },
+  { type: ItemType.Furniture,     icon: 'chair' },
+  { type: ItemType.Prop,          icon: 'extension' },
+  { type: ItemType.Emote,         icon: 'sentiment_satisfied' },
+  { type: ItemType.Stance,        icon: 'accessibility_new' },
+  { type: ItemType.Call,          icon: 'campaign' },
+  { type: ItemType.Music,         icon: 'music_note' },
+  { type: ItemType.Spell,         icon: 'auto_fix_high' },
 ];
 
 @Component({
